c9c72540a256afd8c215c7f804d4cbdc213851fc
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-02-13 12:52:29 -0500

    wrote the logic for LoginForm

--- frontend/components/LoginForm.js
+++ frontend/components/LoginForm.js
@@ -8,6 +8,7 @@ const initialFormValues = {
 export default function LoginForm(props) {
   const [values, setValues] = useState(initialFormValues)
   // ✨ where are my props? Destructure them here
+  const { login } = props;
 
   const onChange = evt => {
     const { id, value } = evt.target
@@ -15,11 +16,18 @@ export default function LoginForm(props) {
   }
 
   const onSubmit = evt => {
-    evt.preventDefault()
+    evt.preventDefault();
+    login(values);
+    setValues(initialFormValues);
     // ✨ implement
   }
 
   const isDisabled = () => {
+    if (values.username.trim().length >= 3 && values.password.trim().length >= 8) {
+      return false;
+    } else {
+      return true;
+    }
     // ✨ implement
     // Trimmed username must be >= 3, and
     // trimmed password must be >= 8 for

e9a13847f9b85501ea54751bf7119920783f621c
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-02-13 12:46:22 -0500

    implemented axiosWithAuth

--- frontend/axios/index.js
+++ frontend/axios/index.js
@@ -1,1 +1,15 @@
 // ✨ implement axiosWithAuth
+import axios from 'axios';
+
+const axiosWithAuth = () => {
+    const token = localStorage.getItem('token');
+    
+    return axios.create({
+        headers: {
+            authorization: token
+        },
+        baseURL: 'http://localhost:9000/api'
+    });
+}
+
+export default axiosWithAuth;
